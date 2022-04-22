50e945090bb708c5b9ba1ab6e308e8f3fc3408c2
Francisco <franmendi.fm@gmail.com>
2022-04-21 23:30:18 -0300

    creacion de componente form-consultation y front de form.blade

--- consultations/resources/views/consultation/form.blade.php
+++ consultations/resources/views/consultation/form.blade.php
@@ -1,99 +1,120 @@
+<div class="mt-10 sm:mt-0">
+    <div class="md:grid md:grid-cols-3 md:gap-6">
 
-    <fieldset>
-            <legend>Informacion requerida</legend>
-            <input type="hidden" value="{{ isset($consultation->id)? $consultation->id : '' }}" name="id" id="id">
-            <div>
-                <label for="teacher_id">Profesor</label>
-
-                <input type="text" multiple name="teacher_id" id="teacher_id" list="drawTeachers" placeholder="Ingrese un nombre" value="{{ isset($consultation->teacher_id)? $consultation->teacher_id : '' }}">
-                @error('teacher_id')
-                <p class="text-red-400 text-xs p-1">{{ $message }}</p>
-            @enderror
-                <datalist id="drawTeachers">
+      <div class="mt-5 md:mt-0 md:col-span-2">
+        <form action="#" method="POST">
+          <div class="shadow overflow-hidden sm:rounded-md">
+            <div class="px-4 py-5 bg-white sm:p-6">
+              <div class="grid grid-cols-6 gap-6">
+                <div class="col-span-6 sm:col-span-3">
+                    <input type="hidden" value="{{ isset($consultation->id)? $consultation->id : '' }}" name="id" id="id">
+                  <label for="teacher_id" class="block text-sm font-medium text-gray-700">Profesor</label>
+                  <input type="text" name="teacher_id" id="teacher_id"
+                  value="{{ isset($consultation->teacher_id)? $consultation->teacher_id : '' }}"
+                  autocomplete="techer_id" list="drawTeachers" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                  <datalist id="drawTeachers">
                     @foreach ( $teachers as $teacher)
-                        <option value="{{$teacher->id}}">{{$teacher->firstname.' '.$teacher->lastname}}</option>
+                    <option value="{{$teacher->id}}">{{$teacher->firstname.' '.$teacher->lastname}}</option>
                     @endforeach
-                </datalist>
+                  </datalist>
+                </div>
 
-
-
-            </div>
-
-            <div>
-                <label for="subject_id">Materia</label>
-                <input type="text" multiple name="subject_id" id="subject_id" list="drawSubject" placeholder="Ingrese la materia" value="{{ isset($consultation->subject_id)? $consultation->subject_id: '' }}">
-                @error('subject_id')
-                    <p class="text-red-400 text-xs p-1">{{ $message }}</p>
-                 @enderror
-                <datalist id="drawSubject">
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="subject_id" class="block text-sm font-medium text-gray-700">Materia</label>
+                  <input type="text" name="subject_id" id="subject_id" list="drawSubject"
+                  value="{{ isset($consultation->subject_id)? $consultation->subject_id : '' }}"
+                  autocomplete="subject_id" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                  <datalist id="drawSubject">
                     @foreach ( $subjects as $subject)
                         <option value="{{$subject->id}}">{{$subject->name}}</option>
                     @endforeach
                 </datalist>
+                </div>
+                <div class="col-span-6 sm:col-span-6">
+                  <label for="type" class="block text-sm font-medium text-gray-700">Tipo de consulta</label>
+                  <select id="type"
+                  value="{{ isset($consultation->type)? $consultation->type : '' }}"
+                  name="type" autocomplete="type" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                    <option>Presencial</option>
+                    <option>Virtual</option>
+                    <option>Hibrida</option>
+                  </select>
+                </div>
 
+                <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                  <label for="dayOfWeek" class="block text-sm font-medium text-gray-700">Día de la semana</label>
+                  <input type="text"
+                  value="{{ isset($consultation->dayOfWeek)? $consultation->dayOfWeek : '' }}"
+                  name="dayOfWeek" id="dayOfWeek" list="dayOfWeekList" autocomplete="address-level2" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+
+                  <datalist id="dayOfWeekList">
+
+                    <option value="Lunes">Lunes</option>
+                    <option value="Martes">Martes</option>
+                    <option value="Miercoles">Miercoles</option>
+                    <option value="Jueves">Jueves</option>
+                    <option value="Viernes">Viernes</option>
+                    <option value="Sabado">Sabado</option>
+                    <option value="Domingo">Domingo</option>
+
+                  </datalist>
+                </div>
+
+                <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <label for="time" class="block text-sm font-medium text-gray-700">Horario</label>
+                  <input type="time"
+                  value="{{ isset($consultation->time)? $consultation->time : '' }}"
+                  name="time" id="time"  class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                </div>
+
+                <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                  <label for="postal-code" class="block text-sm font-medium text-gray-700">Lugar</label>
+                  <input type="text"
+                  value="{{ isset($consultation->place)? $consultation->place : '' }}"
+                  name="place" id="place" autocomplete="postal-code" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                </div>
+
+                @if ($modo == 'crear')
+
+                @else
+
+
+                <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+
+                  <label for="email-address" class="block text-sm font-medium text-gray-700">Estado de la consulta</label>
+                  <input type="text" name="email-address" value="activa" id="email-address" autocomplete="email" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" disabled>
+                  </div>
+                  <div class="col-span-6 sm:col-span-3 lg:col-span-2 ">
+                    <label for="email-address" class="block text-sm font-medium text-gray-700">.</label>
+                    <button type="submit" class=" justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Dar de baja</button>
+
+                </div>
+
+                <div class="col-span-6">
+                  <label for="street-address" class="block text-sm font-medium text-gray-700">Razón cancelada</label>
+                  <input type="textarea" name="street-address" id="street-address" autocomplete="street-address" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                </div>
+
+                <div class="col-span-6">
+                  <label for="street-address" class="block text-sm font-medium text-gray-700">Consulta alternativa</label>
+                  <input type="textarea" name="street-address" id="street-address" autocomplete="street-address" placeholder="Ingresar dia, horario, lugar o link de la consulta alternativa" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                </div>
+
+
+                @endif
+              </div>
+            </div>
+            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                @if ($modo == 'crear')
+                <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Guardar</button>
+                @else
+                <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Modificar</button>
+                @endif
 
             </div>
-
-
-        </fieldset>
-
-        <fieldset>
-            <label for="dayOfWeek">Dia de consulta</label>
-            <input type="text" name="dayOfWeek" id="dayOfWeek" list="dayOfWeekList" placeholder="Ingrese un description" value="{{ isset($consultation->dayOfWeek)? $consultation->dayOfWeek : '' }}">
-            @error('dayOfWeek')
-            <p class="text-red-400 text-xs p-1">{{ $message }}</p>
-            @enderror
-            <datalist id="dayOfWeekList">
-
-                <option value="Lunes">Lunes</option>
-                <option value="Martes">Martes</option>
-                <option value="Miercoles">Miercoles</option>
-                <option value="Jueves">Jueves</option>
-                <option value="Viernes">Viernes</option>
-                <option value="Sabado">Sabado</option>
-                <option value="Domingo">Domingo</option>
-
-
-            </datalist>
-
-
-
-            <label for="time">Horario de consulta</label>
-            <input type="time" name="time" id="time" placeholder="Ingrese hora de consulta" value="{{ isset($consultation->time)? $consultation->time : '' }}">
-
-            @error('time')
-            <p class="text-red-400 text-xs p-1">{{ $message }}</p>
-        @enderror
-
-            <br/>
-              <label for="type">Tipo de consulta</label>
-                    <select name="type" id="type" value="{{ isset($consultation->type)? $consultation->type : '' }}">
-                        <option value="Presencial">Presencial</option>
-                        <option  value="Virtual">Virtual</option>
-
-                    </select>
-
-                    @error('type')
-                    <p class="text-red-400 text-xs p-1">{{ $message }}</p>
-                @enderror
-
-
-              <label for="place">Lugar de consulta</label>
-              <input type="text" name="place" id="place" placeholder="Ingrese lugar de consulta"value="{{ isset($consultation->place)? $consultation->place : '' }}" >
-              @error('place')
-                  <p class="text-red-400 text-xs p-1">{{ $message }}</p>
-              @enderror
-
-              <br/>
-              <label for="link">Link de consulta</label>
-              <input type="text" name="link" id="link" placeholder="Ingrese un link" value="{{ isset($consultation->link)? $consultation->link : '' }}">
-
-
-        </fieldset>
-    @if ($modo == 'crear')
-    <button type="submit">Crear</button>
-    @else
-    <button type="submit">Guardar</button>
-    @endif
-
-
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.tailwindcss.com"></script>
